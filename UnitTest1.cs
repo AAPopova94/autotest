49635b3e8b7f708196d99e45acf991af4b4647ff
AAPopova94 <SVPopov93@gmail.com>
2019-12-15 11:21:00 +0300

    done confirm authorize on booking.com

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,10 +11,17 @@ namespace UnitTestProject1
     public class Autorize01
     {
         [TestMethod]
-        public void OpenUrl()
+        [DataRow("https://www.booking.com/")]
+        public void OpenUrl(string url)
         {
             RemoteWebDriver driver = new ChromeDriver(".");
-            driver.Navigate().GoToUrl(@"https://www.booking.com/");
+            driver.Navigate().GoToUrl(url);
+
+            var expected = url + "index.ru.html"; //указываем ожид адрес куда попастть до знака вопроса, как переменную 
+            var actual = driver.Url.Substring(0, driver.Url.IndexOf("?")); //получаем факт адрес (от нуля до знака вопрос)
+            Assert.AreEqual(expected, actual);
+
+            driver.Quit();
         }
 
         [TestMethod]
@@ -30,11 +37,13 @@ namespace UnitTestProject1
             var actual = driver.Url.Substring(0, driver.Url.IndexOf("?")); //получаем факт адрес (от нуля до знака вопрос)
             Assert.AreEqual(expected, actual); //тут можно так а можно ссылками (если без переменных )
             driver.Quit();
-            
+
         }
 
         [TestMethod]
-        public void ConfirmEmail()
+        [DataRow("aapopova94@gmail.com", true)]
+        [DataRow("aaapopova94@gmail.com", false)]
+        public void ConfirmEmail(string myEmail, bool exp)
         {
             RemoteWebDriver driver = new ChromeDriver(".");
             driver.Navigate().GoToUrl(@"https://www.booking.com/");
@@ -44,48 +53,36 @@ namespace UnitTestProject1
 
             var email = driver.FindElementById("username");
             email.Clear();
-            email.SendKeys("aapopova94@gmail.com");
+            email.SendKeys(myEmail);
 
             var btnNext = driver.FindElementByCssSelector("form button[type='submit']");
             btnNext.Click();
+
+            try
+            {
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(ExpectedConditions.ElementExists(By.Id("password")));
+
+            }
+            catch (Exception)
+            {
+                Assert.IsFalse(exp);
+            }
+
+            var expected = "https://account.booking.com/sign-in/password"; //указываем ожид адрес куда попастть до знака вопроса, как переменную 
+            var actual = driver.Url.Substring(0, driver.Url.IndexOf("?")); //получаем факт адрес (от нуля до знака вопрос)
+            Assert.AreEqual(exp, expected == actual); //тут можно так а можно ссылками (если без переменных )
+
+            driver.Quit();
 
 
         }
 
         [TestMethod]
-        public void ConfirmPassword()
-        {
-            RemoteWebDriver driver = new ChromeDriver(".");
-            driver.Navigate().GoToUrl(@"https://www.booking.com/");
-
-            var btnLogin = driver.FindElementById("current_account");
-            btnLogin.Click();
-
-            //пауза-ожидание пока не загрузится 
-            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            //wait.Until(ExpectedConditions.ElementExists(By.Id("username")));
-
-            var email = driver.FindElementById("username");
-            email.Clear();
-            email.SendKeys("aapopova94@gmail.com");
-
-            var btnNext = driver.FindElementByCssSelector("form button[type='submit']");
-            btnNext.Click();
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(ExpectedConditions.ElementExists(By.Id("password")));
-
-            var password = driver.FindElementById("password");
-            password.Clear();
-            password.SendKeys("aapopova94");
-
-            btnNext = driver.FindElementByCssSelector("form button[type='submit']");
-            btnNext.Click();
-
-        }
-
-        [TestMethod]
-        public void ConfirmIncorrectPassword()
+        [DataRow("aapopova94@gmail.com", "aapopova94", true)]
+        [DataRow("aapopova94@gmail.com", "aapopova95", false)]
+        public void ConfirmPassword(string myEmail, string myPassword, bool exp)
         {
             RemoteWebDriver driver = new ChromeDriver(".");
             driver.Navigate().GoToUrl(@"https://www.booking.com/");
@@ -99,7 +96,7 @@ namespace UnitTestProject1
 
             var email = driver.FindElementById("username");
             email.Clear();
-            email.SendKeys("aapopova94@gmail.com");
+            email.SendKeys(myEmail);
 
             var btnNext = driver.FindElementByCssSelector("form button[type='submit']");
             btnNext.Click();
@@ -109,10 +106,26 @@ namespace UnitTestProject1
 
             var password = driver.FindElementById("password");
             password.Clear();
-            password.SendKeys("Test123");
+            password.SendKeys(myPassword);
 
             btnNext = driver.FindElementByCssSelector("form button[type='submit']");
             btnNext.Click();
+
+            var expected = "https://www.booking.com/index.ru.html"; //указываем ожид адрес куда попастть до знака вопроса, как переменную 
+
+            try //метод который ловит исключения. И если все гуд - идет дальше
+            {
+                wait.Until(ExpectedConditions.UrlContains(expected));
+            }
+            catch (Exception) //а если не гуд - обрабатывает это здесь и мы тут назначаем что именно и как он должен отработать
+            {
+                Assert.IsFalse(exp);
+            }
+
+
+            var actual = driver.Url.Substring(0, driver.Url.IndexOf("?")); //получаем факт адрес (от нуля до знака вопрос)
+            Assert.AreEqual(exp, expected == actual); //тут можно так а можно ссылками (если без переменных )
+            driver.Quit();
 
         }
     }
